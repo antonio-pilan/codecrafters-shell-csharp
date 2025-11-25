8ee40cc3d44f8a8ed79527e359a7906fd9c4f2bc
Antonio Pilan <antonio.pilan@maubisarp.local>
2025-11-25 15:46:26 -0300

    Handle invalid commands

--- src/main.cs
+++ src/main.cs
@@ -1,7 +1,15 @@
+using System.Runtime.CompilerServices;
+
 class Program
 {
     static void Main()
     {
         Console.Write("$ ");
+        string? command = Console.ReadLine();
+
+        if (command != null)
+        {
+            Console.WriteLine($"{command}: command not found");
+        }
     }
 }
